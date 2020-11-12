7717a79cb2ad5239c6635eceeec8d72fb271f1a6
Raziye-Lab <reziya709@gmail.com>
2020-11-12 11:24:37 -0500

    new statement added

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,6 +2,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("Welcome Cocomelon");
         //check head
+        System.out.println("Hello Cocomelon");
 
     }
 }
